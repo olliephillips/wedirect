610f2db7997c5e65ef3761ccc3daba7f1b7ec992
Ollie Phillips <oliver@eantics.co.uk>
2018-02-14 21:01:10 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wedirect
+
+Simple, generic 301 redirection server. Redirects non-www URLs to www prefixed version.
---
+++ wedirect.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func main() {
+	// start HTTP server and redirect to www of requested
+	// domain with 301 redirect
+
+	// handler for everything
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.Host, ("www.")) {
+			// not handling www on this server
+			http.NotFound(w, r)
+		} else {
+			// redirect to www version
+			http.Redirect(w, r, "http://www."+r.Host+r.URL.String(), http.StatusMovedPermanently)
+		}
+	})
+
+	// Specify http server defaults
+	srv := &http.Server{
+		Addr:         ":80",
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	log.Println("Starting HTTP server")
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
